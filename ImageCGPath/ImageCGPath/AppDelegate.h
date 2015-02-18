1af98abb222d8d4c22b6a8f06af3562d9179c0e1
Sergey Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2015-02-18 15:17:14 +0200

    Added project.

---
+++ ImageCGPath/ImageCGPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageCGPath
+//
+//  Created by Serg on 2/18/15.
+//  Copyright (c) 2015 Sergey Biloshkurskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageCGPath/ImageCGPath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageCGPath
+//
+//  Created by Serg on 2/18/15.
+//  Copyright (c) 2015 Sergey Biloshkurskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
